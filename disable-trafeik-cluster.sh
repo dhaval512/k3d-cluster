7c989c56f3194814897608a125a523d6ace981ac
dhaval <dhavalshah512@gmail.com>
2024-05-19 23:36:11 +0530

    add k3d cluster

---
+++ disable-trafeik-cluster.sh
@@ -0,0 +1,9 @@
+k3d cluster create ripal-practical \
+    --api-port 127.0.0.1:6554 \
+    --servers 1 \
+    --agents 1 \
+    --image rancher/k3s:latest \
+    --port 30800-30900:30800-30900@server:0 \
+    --port 80:80@loadbalancer \
+    --port 443:443@loadbalancer \
+    --k3s-arg '--disable=traefik@server:*'
